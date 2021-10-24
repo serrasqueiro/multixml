c2f36f9851bca37e1dada8bf27218235f5f960be
Henrique Moreira <h@serrasqueiro.com>
2021-10-24 20:40:30 +0100

    Basic!

--- src/packages/multix/callingcodes.py
+++ src/packages/multix/callingcodes.py
@@ -7,48 +7,74 @@ Phone Calling Codes, within 'multixml'
 
 # pylint: disable=missing-function-docstring
 
-import sys
-import lxml
+#import lxml
 from lxml import etree
+from waxpage.redit import char_map
+from multix.countryascii import ascii_str
 
-def main():
-    main_test("reader", sys.argv[1:])
+PHONE_METADATA = "$MULTI_BASE/aggregates/ggle/libphonenumber/resources/PhoneNumberMetadata.xml"
 
-def main_test(*args):
-    what = args[0]
-    params = args[1]
-    if len(params) != 1:
-        return None
-    xml_input1 = params[0]
-    print("# Reading xml_input1:", xml_input1, "; action:", what)
-    return fetch_phone_number_metadata(xml_input1)
-
-
-def fetch_phone_number_metadata(xml_input1):
+def fetch_phone_number_metadata(xml_input1) -> dict:
+    territory = None
     in_file = xml_input1
     root = etree.fromstring(open(in_file, "r", encoding="utf-8").read())
     did = [elem for elem in root]
     for item in did:
         assert item.tag == "territories", f"Unknown item tag: {item.tag}"
-        fish(item)
-    return 0
+        assert not territory
+        territory = fish(item)
+    return territory
 
-def fish(item):
+def fish(item, debug=0) -> dict:
     idx = 0
-    text = ""
+    last_elem = None
+    territory = {
+        "code-list": [],
+        "name": {},
+        "info": {},
+    }
+    text, twoletter = "", ""
     for elem in item:
-        ### TODO
+        src_line = elem.sourceline
+        pre = f"::: line {src_line}, #{idx}:"
         is_comment = elem.tag is etree.Comment
         if is_comment:
-            
-        print(f"::: #{idx}:", elem.tag, elem.attrib, ".")
-        print("TEXT:", elem.text)
-        print()
+            comment = shorter_comment(elem.text)
+            if last_elem is None or last_elem.text.strip() == "":
+                text = elem.text.strip()
+                astr = text.split(" ")[-1]
+                if len(astr) >= 4 and astr[0] == "(" and astr[-1] == ")":
+                    twoletter = astr[1:-1].strip()
+                    name = text[:-len(twoletter)-2].strip()
+                    assert len(twoletter) >= 2, f"twoletter too short: '{twoletter}'"
+                    territory["code-list"].append(twoletter)
+                    assert twoletter not in territory["name"], f"Duplicate territory twoletter: '{twoletter}'"
+                    territory["name"][twoletter] = ascii_str(name)
+                    territory["info"][twoletter] = []
+                else:
+                    twoletter = ""
+                shown = f"territory:key={twoletter}: {text}" if twoletter else f"territory:?: {comment}"
+            else:
+                shown = comment
+            if debug > 0:
+                print(pre, shown)
+        else:
+            if debug > 0:
+                print(pre, elem.tag, elem.attrib, ".")
+            if twoletter:
+                territory["info"][twoletter].append((elem.tag, elem.attrib, elem))
         idx += 1
-    return True
+        last_elem = elem
+    assert text
+    return territory
 
+def shorter_comment(astr:str) -> str:
+    new = astr.rstrip()
+    new = char_map.simpler_ascii(new, 1)
+    alist = new.split("\n")
+    new = '\\n'.join([lead.strip() for lead in alist])
+    return new
 
 # Main script
 if __name__ == "__main__":
     print("Please import me.")
-    main()
---
+++ src/packages/multix/countryascii.py
@@ -0,0 +1,21 @@
+# (c)2021  Henrique Moreira
+
+""" converts names to simpler ASCII
+
+Dedicated to country names and such.
+
+See also:
+	[1] two letter country: ISO 3166-1 alpha-2
+"""
+
+from  waxpage.redit import char_map
+
+
+def ascii_str(astr:str) -> str:
+    there = astr
+    there = there.replace("\xc5", "A")	# Aland islands (first 'A' is Angstrom, 0xC5)
+    there = there.replace(chr(8217), "'")	# Cote d'Ivoire (right angle), 0x2019
+    return char_map.simpler_ascii(there)
+
+if __name__ == "__main__":
+    print("Please import me.")
